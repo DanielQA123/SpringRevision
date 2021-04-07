339d687f24bc520329bd2be56f2b48ac3312dbd7
DanielQA123 <DAhuchogu@QA.com>
2021-04-07 21:03:04 +0100

    Made changes by building package

---
+++ src/main/java/com/qa/persistance/domain/SchoolDomain.java
@@ -0,0 +1,14 @@
+package com.qa.persistance.domain;
+
+public class SchoolDomain {
+	
+	//Attributes: 
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String favSubject;
+	private Integer age;
+	
+
+}
