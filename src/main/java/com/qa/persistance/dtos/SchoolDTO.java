da820b91b3ac27644e9423954fa41f99e1917fc7
DanielQA123 <DAhuchogu@QA.com>
2021-04-08 11:14:11 +0100

    DTO class

---
+++ src/main/java/com/qa/persistance/dtos/SchoolDTO.java
@@ -0,0 +1,11 @@
+package com.qa.persistance.dtos;
+
+public class SchoolDTO {
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String favSubject;
+	private Integer age;
+
+}
