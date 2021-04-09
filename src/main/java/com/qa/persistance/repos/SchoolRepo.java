cdd8f1c5052f1fbbb95659d2eafa4ca915a7d746
DanielQA123 <DAhuchogu@QA.com>
2021-04-09 12:25:11 +0100

    Connected to the database

--- src/main/java/com/qa/persistance/repos/SchoolRepo.java
+++ src/main/java/com/qa/persistance/repos/SchoolRepo.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.qa.persistance.domain.SchoolDomain;
 
-//@Repository
-//public interface SchoolRepo extends JpaRepository<SchoolDomain, Long> {
-//
-//	//CRUD -> H2 database
-//	
-//}
+@Repository
+public interface SchoolRepo extends JpaRepository<SchoolDomain, Long> {
+
+	//CRUD -> H2 database
+	
+}
