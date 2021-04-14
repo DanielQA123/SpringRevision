8fb7680fd80521ae9738ed3634b1e40082e82ab4
DanielQA123 <DAhuchogu@QA.com>
2021-04-14 12:16:58 +0100

    Setting up one-to-many relationship

---
+++ src/main/java/com/qa/persistance/domain/DistrictDomain.java
@@ -0,0 +1,71 @@
+package com.qa.persistance.domain;
+
+public class DistrictDomain {
+	
+	//Attributes: 
+	private Long id;
+	private String schoolName;
+	private String postcode;
+	private Integer capacity;
+	
+	public DistrictDomain() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DistrictDomain(Long id, String schoolName, String postcode, Integer capacity) {
+		super();
+		this.id = id;
+		this.schoolName = schoolName;
+		this.postcode = postcode;
+		this.capacity = capacity;
+	}
+
+	public DistrictDomain(String schoolName, String postcode, Integer capacity) {
+		super();
+		this.schoolName = schoolName;
+		this.postcode = postcode;
+		this.capacity = capacity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public Integer getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+
+	@Override
+	public String toString() {
+		return "DistrictDomain [id=" + id + ", schoolName=" + schoolName + ", postcode=" + postcode + ", capacity="
+				+ capacity + "]";
+	}
+	
+	
+	
+}
