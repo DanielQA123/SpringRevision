db80f816505148377d20564d1fabc69ce7331b1f
DanielQA123 <DAhuchogu@QA.com>
2021-04-14 14:51:08 +0100

    Adding service method for read

--- src/main/java/com/qa/rest/DistrictController.java
+++ src/main/java/com/qa/rest/DistrictController.java
@@ -4,18 +4,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.service.DistrictService;
+
 @RestController
 @RequestMapping("/district")
 public class DistrictController {
+	
+	private DistrictService service;
 	
 	@GetMapping("/helloWorld")
 	public String helloWorld() {
 		return "Hello world";
 	}
 	
-	//GET
-	//POST
-	//PUT
-	//DELETE
+	//=======
+	//GET:
+	//=======
+	
+	//=======
+	//POST:
+	//=======
+	
+	//=======
+	//PUT:
+	//=======
+	
+	//=======
+	//DELETE:
+	//=======
+	
+	
 
 }
--- src/main/java/com/qa/service/DistrictService.java
+++ src/main/java/com/qa/service/DistrictService.java
@@ -1,9 +1,14 @@
 package com.qa.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qa.persistance.domain.DistrictDomain;
+import com.qa.persistance.dtos.DistrictDTO;
 import com.qa.persistance.repos.DistrictRepo;
 
 @Service
@@ -18,6 +23,40 @@ public class DistrictService {
 		this.repo = repo;
 		this.mapper = mapper;
 	}
+	
+	private DistrictDTO mapToDTO(DistrictDomain model) {
+		return this.mapper.map(model, DistrictDTO.class);
+	}
+	
+	//=======
+	//GET:
+	//=======
+	
+	//READ ALL:
+	
+	public List<DistrictDTO> readAll(){
+		List<DistrictDomain> districtList = this.repo.findAll();
+		List<DistrictDTO> districtListDTO = districtList.stream().map(this::mapToDTO).collect(Collectors.toList());
+		return districtListDTO;
+	}
+	
+	
+	
+	//=======
+	//POST:
+	//=======
+	
+	public DistrictDTO create(DistrictDomain district) {
+		return this.mapToDTO(this.repo.save(district));
+	}
+	
+	//=======
+	//PUT:
+	//=======
+	
+	//=======
+	//DELETE:
+	//=======
 	
 	
 	
